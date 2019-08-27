436f478711f4eccd466e068710d4359d67663f88
Nathaniel Wheeler <contact@nathanielwheeler.com>
2019-08-27 16:55:46 -0600

    Fixed _some_ of house API problems

--- app/Models/House.js
+++ app/Models/House.js
@@ -1,10 +1,12 @@
 export default class House {
     constructor(data) {
+        this._id = data._id
         this.bedrooms = data.bedrooms
         this.bathrooms = data.bathrooms
         this.levels = data.levels
         this.year = data.year
         this.price = data.price
+        this.imgUrl = data.imgUrl
         this.description = data.description
     }
 
--- app/main.js
+++ app/main.js
@@ -1,12 +1,14 @@
 import CarController from "./Controllers/CarController.js";
 import JobController from "./Controllers/JobController.js"
+import HouseController from "./Controllers/HouseController.js";
 
 
 class App {
     constructor() {
         this.controllers = {
             carCtrl: new CarController(),
-            jobCtrl: new JobController()
+            jobCtrl: new JobController(),
+            houseCtrl: new HouseController()
         }
     }
 }
